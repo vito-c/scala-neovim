00e7c1b00c54df7abb431bd0f2c3e2bd9fb50548
GodFather <vito.blog@gmail.com>
2020-08-31 01:40:48 -0700

    cleanup

--- server/src/main/scala/nvim/v1/Position.scala
+++
@@ -1,5 +0,0 @@
-// package nvim.v1
-//
-// final case class Position(row: Int, col: Int) {
-//   override def toString = s"[row=$row,col=$col]"
-// }
--- server/src/main/scala/nvim/v1/protocol/UnexpectedResponse.scala
+++
@@ -1,3 +0,0 @@
-// package nvim.v1.protocol
-//
-// final class UnexpectedResponse(response: String) extends RuntimeException(s"Request can't handle `$response`.")
